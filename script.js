fc7ef58ec82496f4975b5a1460efd0787588e94d
Aldair <djaldair14@gmail.com>
2023-05-29 19:58:17 -0500

    doing changes

--- script.js
+++ script.js
@@ -1,23 +1,3 @@
-const form = document.getElementById("myForm");
-form.addEventListener("submit", function (event) {
-  event.preventDefault();
-  validateForm();
-});
-
-function validateEmail(email) {
-  const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,7}$/;
-  return regex.test(email);
-}
-function validateForm() {
-  const emailInput = document.getElementById("email");
-  const email = emailInput.value;
-  if (!validateEmail(email)) {
-    alert("Por favor ingrese un correo electrónico válido.");
-  } else {
-    alert("Correo electrónico enviado correctamente.");
-  }
-}
-
 document
   .querySelector("button.button-menu-toggle")
   .addEventListener("click", function () {
@@ -25,3 +5,30 @@ document
       .querySelector(".nav-links")
       .classList.toggle("nav-links-responsive");
   });
+
+// const form = document.getElementById("myForm");
+// form.addEventListener("submit", function (event) {
+//   event.preventDefault();
+//   validateForm();
+// });
+
+// function validateEmail(email) {
+//   const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,7}$/;
+//   return regex.test(email);
+// }
+// function validateForm() {
+//   const emailInput = document.getElementById("email");
+//   const email = emailInput.value;
+//   if (!validateEmail(email)) {
+//     alert("Por favor ingrese un correo electrónico válido.");
+//   } else {
+//     alert("Correo electrónico enviado correctamente.");
+//   }
+// }
+
+document.querySelector("#contact-form").addEventListener("submit", (e) => {
+  e.preventDefault();
+  e.target.elements.name.value = "";
+  e.target.elements.email.value = "";
+  e.target.elements.message.value = "";
+});
